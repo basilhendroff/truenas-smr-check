c8a51a32261a975f9c66f2e63af88f60bd732d3f
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2021-06-27 05:39:28 +0800

    Update smr-check.sh
    Improve readibility

--- smr-check.sh
+++ smr-check.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-DP=7
-TP=10
-SP=17
+# Table column widths
+DeviceWidth=7
+ModelWidth=10
+SerialWidth=17
 
-cols="%${DP}s | %${TP}s | %${SP}s |\n"
+cols="%${DeviceWidth}s | %${ModelWidth}s | %${SerialWidth}s |\n"
 
-# Get list of disks and create arrays
+# Gather disk information and populate arrays
 midclt call disk.query | jq -S '.[] | {devname: .devname, model: .model, serial: .serial}' > tmp.json
 
-declare -a "DEV=($(<tmp.json jq -r '.devname | @sh'))"
-declare -a "MOD=($(<tmp.json jq -r '.model | @sh'))"
-declare -a "SN=($(<tmp.json jq -r '.serial | @sh'))"
+declare -a "Device=($(<tmp.json jq -r '.devname | @sh'))"
+declare -a "Model=($(<tmp.json jq -r '.model | @sh'))"
+declare -a "Serial=($(<tmp.json jq -r '.serial | @sh'))"
 
 rm tmp.json
 
-function check() {
+# Detection engine
+# $1 = Name of array
+# $2 = Any character to trigger silent mode
+function DetectSMR() {
 name=$1[@]
-
 SMR=("${!name}")
-# Across all disks
-for ((i=0;i<${#DEV[@]};i++)); do
-
-# if a disk is a WD
-#  if [[ "${MOD[$i]:0:6}" == "${SMR[0]}" ]]; then
-  if [[ "${MOD[$i]}" == *"${SMR[0]}"* ]]; then
-    TMP[$i]="${MOD[$i]#*" "}"
+# For each disk
+for ((i=0;i<${#Device[@]};i++)); do
+# Is it a WD disk?
+  if [[ "${Model[$i]}" == *"${SMR[0]}"* ]]; then
+# If so, Add the trimmed model name to a temporary array  
+    TMP[$i]="${Model[$i]#*" "}"
     TMP[$i]="${TMP[$i]%%-*}"
-
-# if it is a suspect model
+# Based on the model suffix...
     if [[ "${TMP[$i]}" == *"${SMR[-1]}"* ]]; then
       for ((j=1;j<$((${#SMR[@]}-1));j++)); do
-
-# if it is a suspect size
+# Look for an SMR model
         if [[ "${TMP[$i]}" == *"${SMR[j]}"* ]]; then
           if [[ -z "$2" ]]; then  
-            printf "$cols" "${DEV[$i]}" "${TMP[$i]}" "${SN[$i]}"
+            printf "$fmt" "${Device[$i]}" "${TMP[$i]}" "${Serial[$i]}"
           else
-            flag=1
+            f=1
           fi
         fi
       done
     fi
-#    printf "%5s %10s %20s\n" "${DEV[$i]}" "${TMP[$i]}" "${SN[$i]}"
+#    printf "%5s %10s %20s\n" "${Device[$i]}" "${TMP[$i]}" "${Serial[$i]}"
   fi
 done
 }
@@ -54,38 +54,36 @@ done
 # 2. https://nascompares.com/answer/list-of-wd-cmr-and-smr-hard-drives-hdd/
 # 3. https://hddscan.com/blog/2020/hdd-wd-smr.html
 #
-SMR01=("WDC WD" "20" "30" "40" "60" "EFAX")	#1
-SMR02=("WDC WD" "20" "30" "40" "60" "EZAZ")	#1
-SMR03=("WDC WD" "10" "20" "SPZX")		#1
-SMR04=("WDC WD" "10" "SPSX")			#1
-SMR05=("WDC WD" "9000" "LPZX")			#1
-SMR06=("WDC WD" "5000" "LPSX")			#2
-SMR07=("WDC WD" "40" "60" "EDAZ")		#3
-SMR08=("WDC WD" "60" "EMAZ")			#3
-SMR09=("WDC WD" "10" "SPWX")			#3
-SMR10=("WDC WD" "40" "NPZZ")			#3
+WD01=("WDC WD" "20" "30" "40" "60" "EFAX")	#1
+WD02=("WDC WD" "20" "30" "40" "60" "EZAZ")	#1
+WD03=("WDC WD" "10" "20" "SPZX")		        #1
+WD04=("WDC WD" "10" "SPSX")			            #1
+WD05=("WDC WD" "9000" "LPZX")			          #1
+WD06=("WDC WD" "5000" "LPSX")			          #2
+WD07=("WDC WD" "40" "60" "EDAZ")		        #3
+WD08=("WDC WD" "60" "EMAZ")			            #3
+WD09=("WDC WD" "10" "SPWX")			            #3
+WD10=("WDC WD" "40" "NPZZ")			            #3
 
+TEST=("WDC WD" "30" "EFRX")
 
+f=0
+cols="%${DeviceWidth}s | %${ModelWidth}s | %${SerialWidth}s |\n"
 
+DetectSMR WD01 quiet
+DetectSMR WD02 quiet
+DetectSMR WD03 quiet
+DetectSMR WD04 quiet
+DetectSMR WD05 quiet
+DetectSMR WD06 quiet
+DetectSMR WD07 quiet
+DetectSMR WD08 quiet
+DetectSMR WD09 quiet
+DetectSMR WD10 quiet
 
-SMRX=("WDC WD" "30" "EFRX")
+DetectSMR TEST quiet
 
-flag=0
-
-check SMR01 test
-check SMR02 test
-check SMR03 test
-check SMR04 test
-check SMR05 test
-check SMR06 test
-check SMR07 test
-check SMR08 test
-check SMR09 test
-check SMR10 test
-
-check SMRX test
-
-if [[ "$flag" == 0 ]]; then
+if [[ "$f" == 0 ]]; then
   echo
   echo -e "\e[1;32mNo known WD SATA SMR disks detected.\e[0m"
   echo
@@ -94,20 +92,22 @@ else
   echo -e "\e[1;31mKnown WD SATA SMR disk(s) detected.\e[0m"
   echo
 
-  printf "$cols" "Device" "Model" "Serial Number" 
-  s=$(printf "%-$((DP+TP+SP+8))s" "-")
+  fmt="%${DeviceWidth}s | %${ModelWidth}s | %${SerialWidth}s |\n"
+  
+  printf "$fmt" "Device" "Model" "Serial Number" 
+  s=$(printf "%-$((DeviceWidth+ModelWidth+SerialWidth+8))s" "-")
   echo "${s// /-}"
 
-  check SMR01
-  check SMR02
-  check SMR03
-  check SMR04
-  check SMR05
-  check SMR06
-  check SMR07
-  check SMR08
-  check SMR09
-  check SMR10
+  DetectSMR WD01
+  DetectSMR WD02
+  DetectSMR WD03
+  DetectSMR WD04
+  DetectSMR WD05
+  DetectSMR WD06
+  DetectSMR WD07
+  DetectSMR WD08
+  DetectSMR WD09
+  DetectSMR WD10
 
-  check SMRX
+  DetectSMR TEST
 fi
