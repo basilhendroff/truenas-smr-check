9cd3d91229b4d1506b9d23b5a1c46c17569c274f
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2021-06-27 16:04:36 +0800

    Update smr-check.sh
    1. Check for Seagate SMR disks
    2. Improvement to coding - Loop through arrays

--- smr-check.sh
+++ smr-check.sh
@@ -18,19 +18,22 @@ rm tmp.json
 # $1 = Name of array
 # $2 = Any character to trigger silent mode
 function DetectSMR() {
+# Initialise
 name=$1[@]
 SMR=("${!name}")
+unset TMP
+
 # For each disk
 for ((i=0;i<${#Device[@]};i++)); do
-# Is it a WD disk?
-  if [[ "${Model[$i]}" == *"${SMR[0]}"* ]]; then
-# If so, Add the trimmed model name to a temporary array  
+# Is the manufacturer in the database?
+  if [[ "${Model[$i]:0:6}" == *"${SMR[0]}"* ]]; then
+# If so, add the trimmed model name to a temporary array  
     TMP[$i]="${Model[$i]#*" "}"
     TMP[$i]="${TMP[$i]%%-*}"
 # Based on the model suffix...
     if [[ "${TMP[$i]}" == *"${SMR[-1]}"* ]]; then
       for ((j=1;j<$((${#SMR[@]}-1));j++)); do
-# Look for an SMR model
+# Is the model in the SMR database?
         if [[ "${TMP[$i]}" == *"${SMR[j]}"* ]]; then
           if [[ -z "$2" ]]; then  
             printf "$fmt" "${Device[$i]}" "${TMP[$i]}" "${Serial[$i]}"
@@ -44,23 +47,46 @@ for ((i=0;i<${#Device[@]};i++)); do
 done
 }
 
-# Define SMR arrays
+# Define SMR arrays (sorted by suffix)
 #
 # References:
 # 1. https://www.truenas.com/community/resources/list-of-known-smr-drives.141/
 # 2. https://nascompares.com/answer/list-of-wd-cmr-and-smr-hard-drives-hdd/
 # 3. https://hddscan.com/blog/2020/hdd-wd-smr.html
 #
-WD01=("WDC WD" "20" "30" "40" "60" "EFAX")  #1
-WD02=("WDC WD" "20" "30" "40" "60" "EZAZ")  #1
-WD03=("WDC WD" "10" "20" "SPZX")            #1
-WD04=("WDC WD" "10" "SPSX")                 #1
-WD05=("WDC WD" "9000" "LPZX")               #1
-WD06=("WDC WD" "5000" "LPSX")               #2
-WD07=("WDC WD" "40" "60" "EDAZ")            #3
-WD08=("WDC WD" "60" "EMAZ")                 #3
+# Western Digital
+WD01=("WDC WD" "40" "60" "EDAZ")            #3
+WD02=("WDC WD" "20" "30" "40" "60" "EFAX")  #1
+WD03=("WDC WD" "60" "EMAZ")                 #3
+WD04=("WDC WD" "20" "30" "40" "60" "EZAZ")  #1
+WD05=("WDC WD" "5000" "LPSX")               #2
+WD06=("WDC WD" "9000" "LPZX")               #1
+WD07=("WDC WD" "40" "NPZZ")                 #3
+WD08=("WDC WD" "10" "SPSX")                 #1
 WD09=("WDC WD" "10" "SPWX")                 #3
-WD10=("WDC WD" "40" "NPZZ")                 #3
+WD10=("WDC WD" "10" "20" "SPZX")            #1
+
+# Seagate
+ST01=("ST" "6000" "8000" "AS0002")          #1
+ST02=("ST" "8000" "AS0003")                 #1
+ST03=("ST" "5000" "AS0011")                 #1
+ST04=("ST" "5000" "DM000")                  #1
+ST05=("ST" "5000" "6000" "DM003")           #1
+ST06=("ST" "4000" "8000" "DM004")           #1
+ST07=("ST" "2000" "DM005")                  #1
+ST08=("ST" "3000" "DM007")                  #1
+ST09=("ST" "2000" "DM008")                  #1
+ST10=("ST" "8000" "VX002")                  #2
+ST11=("ST" "4000" "VX005")                  #2
+ST12=("ST" "2000" "VX007")                  #1
+ST13=("ST" "1000" "8000" "VX008")           #1, #2
+ST14=("ST" "6000" "VX010")                  #2
+ST15=("ST" "6000" "VX011")                  #2
+ST16=("ST" "2000" "VX012")                  #2
+ST17=("ST" "4000" "VX013")                  #2
+ST18=("ST" "2000" "VX015")                  #2
+ST19=("ST" "3000" "VX016")                  #2
+ST20=("ST" "3000" "VX017")                  #2
 
 # To test this script when you have no SMR disks, configure the TEST array below with a valid CMR disk on your system.
 # Temporarily uncomment the three TEST lines below and run the script. 
@@ -69,16 +95,16 @@ WD10=("WDC WD" "40" "NPZZ")                 #3
 # Quiet detection phase. If an SMR disk is detected flag f will be set to 1.
 f=0
 
-DetectSMR WD01 q
-DetectSMR WD02 q
-DetectSMR WD03 q
-DetectSMR WD04 q
-DetectSMR WD05 q
-DetectSMR WD06 q
-DetectSMR WD07 q
-DetectSMR WD08 q
-DetectSMR WD09 q
-DetectSMR WD10 q
+# Detect Western Digital SMR disks
+for k in {01..10}; do
+  DetectSMR WD"$k" q
+done
+
+# Detect Seagate SMR disks
+for k in {01..20}; do
+  DetectSMR ST"$k" q
+done
+
 # DetectSMR TEST q
 
 # If the flag f is still zero, no SMR disk was detected. :)
@@ -98,16 +124,16 @@ else
   s=$(printf "%-$((DeviceWidth+ModelWidth+SerialWidth+8))s" "-")
   echo "${s// /-}"
 
-  DetectSMR WD01
-  DetectSMR WD02
-  DetectSMR WD03
-  DetectSMR WD04
-  DetectSMR WD05
-  DetectSMR WD06
-  DetectSMR WD07
-  DetectSMR WD08
-  DetectSMR WD09
-  DetectSMR WD10
+# Detect Western Digital SMR disks
+  for k in {01..10}; do
+    DetectSMR WD"$k"
+  done
+
+# Detect Seagate SMR disks
+  for k in {01..20}; do
+    DetectSMR ST"$k"
+  done
+
 # DetectSMR TEST  
   echo
 fi
